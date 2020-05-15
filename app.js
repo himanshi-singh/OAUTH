db3f222851b24fa4440cd5de7d5f18d6efc54147
Your Name <you@example.com>
2020-05-15 20:36:18 +0530

    third commit

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const passport=require("passport");
 const local=require("passport-local");
 const passportLocalMongoose=require("passport-local-mongoose");
 const session = require('express-session');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
+
 const app = express();
 /*const bcrypt = require('bcryptjs');
 const salt = bcrypt.genSaltSync(10);*/
@@ -26,21 +29,56 @@ app.use(passport.session());
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true,useUnifiedTopology: true,useCreateIndex:true});
 const userSchema =new mongoose.Schema( {
   email: String,
-  password: String
+  password: String,
+  googleId:String
 });
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 const User=mongoose.model("User",userSchema);
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
 //mongoose.set('useFindAndModify', false);
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.SECRET_ID,
+    callbackURL: "https://localhost:3000/auth/google/secrets"
+  },
+  // In this example, the user's Facebook profile is supplied as the user
+    // record.  In a production-quality application, the Facebook profile should
+    // be associated with a user record in the application's database, which
+    // allows for account linking and authentication with other identity
+    // providers.
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+
 
 app.get("/",function(req,res){
   res.render("home");
 })
+app.get("/auth/google",passport.authenticate("google", { scope: ["profile"] }));
 app.get("/register",function(req,res){
   res.render("register");
 })
+app.get("/auth/google/secrets",
+  passport.authenticate("google", { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
 app.get("/login",function(req,res){
   res.render("login");
 })
